5b9e6fe647f39281e611a474fb46845b68b2d2c4
xatomen <jorgito899@gmail.com>
2023-06-09 00:02:43 -0400

    Actualización 0:02 09-06-2023

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 
 //AÑADIR LIBRERIA TIME
 
@@ -60,6 +61,9 @@ void generar_listado_aleatorio(int length, int min){
     FILE *archivo;
     char nombre_archivo[50] = "listado_alteatorio_length_";
     char str[20];
+    int value;
+
+    srand(time(NULL));
 
     sprintf(str,"%d",length);
     strcat(nombre_archivo,str);
@@ -70,9 +74,10 @@ void generar_listado_aleatorio(int length, int min){
     archivo = fopen(nombre_archivo, "w");
     char cadena[10];
 
-    for(int i=0; i+min<=length+min; i++){
+    for(int i=0; i<=length; i++){
         /*HACER EL RANDOM PARA COMPLETAR EL LISTADO*/
-        sprintf(cadena,"%d",i);
+        value = rand() % length + min;
+        sprintf(cadena,"%d",value);
         fputs(cadena,archivo);
         fputs("\n",archivo);
     }
@@ -81,51 +86,196 @@ void generar_listado_aleatorio(int length, int min){
 
 }
 
-int cargar_listado_en_arreglo(int array[]){
+// int cargar_listado_en_arreglo(int array[]){
+//     char name[50];
+//     printf("Ingrese el nombre del archivo\n");
+//     scanf("%49s",name);
 
-    //LEER ARCHIVO Y GUARDAR EN ARREGLO
+//     //LEER ARCHIVO Y GUARDAR EN ARREGLO
 
-    return array;
+//     // FILE *archivo;
+    
+
+//     /*Leemos el tamaño del arreglo a partir del nombre del archivo*/
+//     char str[]="h_";
+//     // char sub_str[3]="";
+//     char length[21];
+//     // char character;
+//     for(int i=0;i<=20;i++){
+//         // character = name[i];
+//         // strcat(sub_str,character);
+//         // character = name[i+1];
+//         // strcat(sub_str,character);
+//         char sub_str[3] = name[i] + name[i+1];
+//         if(strcmp(sub_str,str)==1){
+//             for(int j=i+2;j<=20;j++){
+//             //    strcat(length,name[j]);
+//                 length[21] = length + name[j];
+//                 if(name[j+1]=='_'){
+//                 break;
+//                }
+//             }
+//             break;
+//         }
+//     }
+//     printf("%s",length);
+//     return 0;
+// }
+
+// int cargar_listado_en_arreglo(int array[]) {
+//     char name[50];
+//     printf("Ingrese el nombre del archivo:\n");
+//     scanf("%49s", name); // Limitamos la entrada a 49 caracteres para evitar desbordamientos de búfer
+
+//     // Leer archivo y guardar en arreglo
+
+//     char str[] = "h_";
+//     char sub_str[3] = ""; // Se reserva espacio para dos caracteres más el carácter nulo
+//     char length[21] = ""; // Se reserva espacio para 20 caracteres más el carácter nulo
+    
+//     for (int i = 0; i <= 20; i++) {
+//         strncat(sub_str, &name[i], 1);
+        
+//         if (strcmp(sub_str, str) == 0) {
+//             for (int j = i + 2; j <= 20; j++) {
+//                 strncat(length, &name[j], 1);
+//                 if (name[j + 1] == '_') {
+//                     break;
+//                 }
+//             }
+//             break;
+//         }
+//     }
+    
+//     printf("%s\n", length);
+//     printf("hola");
+    
+//     return 0;
+// }
+
+int cargar_listado_en_arreglo(int array[]) {
+    char name[50];
+    printf("Ingrese el nombre del archivo:\n");
+    scanf("%49s", name);
+
+    /*Obtenemos el largo del array*/
+    char str[20];
+    for(int i=0; i<=50; i++){
+        if(name[i]=='h'){
+            printf("h encontrado\n");
+            for(int j=i+2; j<=50; j++){
+                printf("iteracion...\n");
+                if(name[j]=='_'){
+                    break;
+                }
+                else{
+                    str[j-i-2]=name[j];
+                }
+            }
+        }
+    }
+    int length = atoi(str);
+
+    /*Declaramos el array con el tamaño correspondiente*/
+    int arreglo[length];
+    int i=0;
+    FILE *archivo;
+    archivo = fopen(name,"r");
+    char buffer[100];
+    while(fgets(buffer,100,archivo)){
+        strtok(buffer,"\n");
+        arreglo[i]=atoi(buffer);
+        i++;
+    }
+    fclose(archivo);
+
+    for(int i=0; i<=length; i++){
+        printf("indice: %d - dato: %d\n",i,arreglo[i]);
+    }
+
+    return 0;
 }
 
 
 /*Programa principal*/
 int main(){
-    int opcion=1, length=0, min=0;
-    while(opcion!=0){
+    printf("hola");
+    int opcion_menu=1;
+    int opcion_menu_generar=1, opcion_menu_buscar=1, opcion_menu_ordenar=1, length=0, min=0;
+    int array[1000];
+    printf("hola");
+    // char name[50];
+    while(opcion_menu!=0){
         printf("Menu:\n");
-        printf("1. Generar listado numerico ordenado ascendente\n");
-        printf("2. Generar listado numerico ordenado descendente\n");
-        printf("3. Generar listado numerico aleatorio\n");
+        printf("1. Generar listado\n");
+        printf("2. Cargar listado en array\n");
+        printf("3. Buscar en array\n");
+        printf("4. Ordenar array\n");
         printf("0. Salir\n");
         printf("Ingrese una opcion: \n");
-        scanf("%d",&opcion);
-        switch(opcion){
+        scanf("%d",&opcion_menu);
+        
+        switch(opcion_menu){
             case 0:
-
             break;
 
+            /*Submenú para generar listados*/
             case 1:
-                /*Completamos los datos para ingresarlos como parámetros*/
-                printf("Ingrese el largo del arreglo");
-                scanf("%d",&length);
-                printf("Ingrese el valor mínimo");
-                scanf("%d",&min);
-                /*Invocamos la función*/
-                generar_listado_asc(length, min);
+                printf("Menu generar listado:\n");
+                printf("1. Generar listado numerico ordenado ascendente\n");
+                printf("2. Generar listado numerico ordenado descendente\n");
+                printf("3. Generar listado numerico aleatorio\n");
+                printf("0. Salir\n");
+                printf("Ingrese una opcion: \n");
+                scanf("%d",&opcion_menu_generar);
+
+                switch(opcion_menu_generar){
+
+                    /*Salir*/
+                    case 0:
+                    break;
+
+                    case 1:
+                        /*Completamos los datos para ingresarlos como parámetros*/
+                        printf("Ingrese el largo del arreglo\n");
+                        scanf("%d",&length);
+                        printf("Ingrese el valor minimo\n");
+                        scanf("%d",&min);
+                        /*Invocamos la función*/
+                        generar_listado_asc(length, min);
+                    break;
+
+                    case 2:
+                        /*Completamos los datos para ingresarlos como parámetros*/
+                        printf("Ingrese el largo del arreglo\n");
+                        scanf("%d",&length);
+                        printf("Ingrese el valor minimo\n");
+                        scanf("%d",&min);
+                        /*Invocamos la función*/
+                        generar_listado_desc(length, min);
+                    break;
+
+                    case 3:
+                        /*Completamos los datos para ingresarlos como parámetros*/
+                        printf("Ingrese el largo del arreglo\n");
+                        scanf("%d",&length);
+                        printf("Ingrese el valor minimo\n");
+                        scanf("%d",&min);
+                        /*Invocamos la función*/
+                        generar_listado_aleatorio(length, min);
+                    break;
+            }
             break;
 
             case 2:
-                /*Completamos los datos para ingresarlos como parámetros*/
-                printf("Ingrese el largo del arreglo");
-                scanf("%d",&length);
-                printf("Ingrese el valor mínimo");
-                scanf("%d",&min);
-                /*Invocamos la función*/
-                generar_listado_desc(length, min);
+                
+                cargar_listado_en_arreglo(array);
             break;
 
             case 3:
+            break;
+
+            case 4:
             break;
         }
     }
